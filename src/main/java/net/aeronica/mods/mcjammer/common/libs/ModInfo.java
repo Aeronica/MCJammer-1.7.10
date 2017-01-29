40a123bf81cdd8e8bcbc7f081ed31e44595bc333
Aeronica <pmboese@gmail.com>
2017-01-29 13:09:04 -0600

    Add license and readme. Update build date.

--- src/main/java/net/aeronica/mods/mcjammer/common/libs/ModInfo.java
+++ src/main/java/net/aeronica/mods/mcjammer/common/libs/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo
 {
     public static final String ID = "MCJammer";
     public static final String NAME = "MC Jammer";
-    public static final String VERSION = "1.7.10-2016.03.20";
+    public static final String VERSION = "1.7.10-2017.01.29";
     public static final String CHANNEL = ID;
 
     public static final String COMMON_PROXY = "net.aeronica.mods.mcjammer.common.core.";
